cecfff70339e467e7d7e4ae6a27670d1ccd7f4e1
Haroun B <haroun@harryb.dev>
2022-06-11 20:51:39 +0200

    adds dependencies ;connects app to db; and starts server

---
+++ index.js
@@ -0,0 +1,31 @@
+const app = require('express')(),
+  logger = require('morgan');
+
+
+// provides access to environment variables/settings
+require('dotenv/config');
+
+// connects to the database
+require('./db/connect')();
+
+// provides logging in the development environment
+app.use(logger('dev'));
+
+// provides access to the `body` property in the request
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// adds routing
+app.use(`/crates`, require(`./routes/crates.router`));
+app.use(`/bottles`, require(`./routes/bottles.router`));
+app.use(`/user`, require(`./routes/user.router`));
+
+// adds error handling
+app.use(require(`./routes/error-handling.router`));
+
+
+// starts the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
