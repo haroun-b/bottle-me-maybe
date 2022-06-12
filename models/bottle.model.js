df518def5a5e8abcf742c9b855591a260664dfd0
Haroun B <haroun@harryb.dev>
2022-06-12 13:47:20 +0200

    adds db schema models

---
+++ models/bottle.model.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require(`mongoose`);
+
+
+const bottleSchema = new Schema(
+  {
+    authorId: {
+      type: Schema.Types.ObjectId,
+      ref: `User`,
+      required: true
+    },
+    crateId: {
+      type: Schema.Types.ObjectId,
+      ref: `Crate`,
+      required: true
+    },
+    message: {
+      type: String,
+      required: true
+    },
+    latestFetch: {
+      userId: {
+        type: Schema.Types.ObjectId,
+        ref: `User`
+      },
+      time: Date
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Bottle = model(`Bottle`, bottleSchema);
+
+module.exports = Bottle;
+
+
