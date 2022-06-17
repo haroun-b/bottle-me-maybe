8b6ececdb8b4efacc0465b7ca2f8bc55ba399970
Haroun B <81895619+haroun-b@users.noreply.github.com>
2022-06-17 11:32:32 +0000

    adds seeds for testing (#26)
    * updates the replyPath
    
    * debug sess ip
    
    * debug sess ip
    
    * removes ::ffff: from IPv4
    
    * sorts views in desc order by createdAt
    
    * debug sess ip
    
    * debug sess ip
    
    * debug sess ip
    
    * debug sess ip
    
    * debug sess ip
    
    * adds seeds for testing

---
+++ tests/seed.data.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    message: `message 1`
+  },
+  {
+    message: `message 2`
+  },
+  {
+    message: `message 3`
+  },
+  {
+    message: `message 4`
+  },
+  {
+    message: `message 5`
+  },
+  {
+    message: `message 6`
+  }
+];
